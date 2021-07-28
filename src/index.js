2ec4112bf37389bece7b20701f221210fb956aa8
garrettbland <gmorganbland@gmail.com>
2021-07-28 01:19:29 -0500

    working on jsdom and modifying forms

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
  * Plugin that runs before netlify build to add
  * netlify form attributes to <form>. Used to convert basic html
  * forms to let netlify handle form submissions.
+ * https://docs.netlify.com/configure-builds/build-plugins/create-plugins
  */
+const glob = require('glob')
 const fs = require('fs')
 
 module.exports = {
-    onPreBuild: ({ constants, utils }) => {
+    onBuild: ({ constants, utils }) => {
         try {
             /**
              * Show a message to the user that the plugin is starting
@@ -15,15 +17,38 @@ module.exports = {
 
             /**
              * Step 1.
-             * Reference static files
-             * Since this is in the pre-build stage, it may not exist yet
+             * Reference built html pages
              */
             const SITE_DIR = constants.PUBLISH_DIR
 
             /**
-             * Step 2.
-             * Check to see if files exist in the published directory
+             * Returns path and file name like below
+             * ['static_build/index.html','static_build/subfolder/test.html']
              */
+            var get_html_files = function (src) {
+                return glob.sync(src + '/**/*.html')
+            }
+
+            /**
+             * Get HTML files and content
+             */
+            var files = get_html_files(SITE_DIR, function (err, res) {
+                if (err) {
+                    throw Error('Glob Error', err)
+                } else {
+                    res.forEach(function (html_file) {
+                        console.log(`File: ${html_file}`)
+                        fs.readFile(html_file, 'utf8', function (err, data) {
+                            if (err) {
+                                throw Error('File Read Error', err)
+                            } else {
+                                // readFile returns a Buffer object, which we can then convert to string
+                                return data.toString()
+                            }
+                        })
+                    })
+                }
+            })
 
             /**
              * Step 3.
--- test.js
+++ test.js
@@ -1,26 +1,83 @@
 const glob = require('glob')
 const fs = require('fs')
+const { JSDOM } = require('jsdom')
 
 // Returns array of html files
 // Eg => ['static_test/index.html','static_test/subfolder/test.html']
-var get_html_files = function (src, callback) {
-    glob(src + '/**/*.html', callback)
+var get_html_files = function (src) {
+    return glob.sync(src + '/**/*.html')
+    // glob(src + '/**/*.html', callback)
 }
 
-get_html_files('static_test', function (err, res) {
-    if (err) {
-        console.log('Glob Error', err)
-    } else {
-        res.forEach(function (html_file) {
-            console.log(`File: ${html_file}`)
-            fs.readFile(html_file, 'utf8', function (err, data) {
-                if (err) {
-                    return console.log('File Read Error', err)
-                } else {
-                    // readFile returns a Buffer object, which we can then convert to string
-                    console.log(data.toString())
-                }
+// var files = get_html_files('static_test', function (err, res) {
+//     if (err) {
+//         console.log('Glob Error', err)
+//     } else {
+//         return res.map(function (html_file) {
+//             console.log(`File: ${html_file}`)
+//             fs.readFile(html_file, 'utf8', function (err, data) {
+//                 if (err) {
+//                     return console.log('File Read Error', err)
+//                 } else {
+//                     // readFile returns a Buffer object, which we can then convert to string
+//                     //console.log(data.toString())
+//                     return data.toString()
+//                 }
+//             })
+//         })
+//     }
+// })
+
+// returns array of files with html content
+get_html_files('static_test').forEach((html_file) => {
+    console.log('Reading files...')
+    fs.readFile(html_file, 'utf8', (err, data) => {
+        console.log('File read...')
+        if (err) {
+            throw Error('File Read Error', err)
+        } else {
+            // readFile returns a Buffer object, which we can then convert to string
+            //return data.toString()
+            const string_content = data.toString()
+
+            const dom = new JSDOM(string_content)
+            const document = dom.window.document
+
+            const [...forms] = document.getElementsByTagName('form')
+
+            forms.forEach((form, index) => {
+                console.log('Updating dom...')
+                form.setAttribute('name', `${document.title} ${index + 1}`)
+                form.setAttribute('data-netlify', 'true')
             })
-        })
-    }
+
+            if (forms.length > 0) {
+                fs.writeFile(html_file, dom.serialize(), (err) => {
+                    if (err) {
+                        throw Error('Error writing file...')
+                    } else {
+                        console.log('Updated file...')
+                    }
+                })
+            }
+        }
+    })
 })
+
+// go through each html file and check for form tag
+
+// html_files.forEach((file) => {
+//     const dom = new JSDOM(file)
+//     const document = dom.window.document
+
+//     const [...forms] = document.getElementsByTagName('form')
+
+//     forms.forEach((form, index) => {
+//         form.setAttribute('name', `${document.title} ${index + 1}`)
+//         form.setAttribute('data-netlify', 'true')
+//     })
+
+//     return dom.serialize()
+// })
+
+// console.log(html_files_with_forms)
